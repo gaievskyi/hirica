eaddf0d83d30833cccf9e41279fbcfdcaca24cc3
Daniel Gajewski <daniel.gajewski@iteo.com>
2023-01-05 13:36:33 +0100

    Fix literówkę

--- src/ui/modules/profile-head/profile-head.tsx
+++ src/ui/modules/profile-head/profile-head.tsx
@@ -2,7 +2,7 @@ export const ProfileHead: React.FC = () => (
   <div className="pb-24 lg:flex lg:items-center lg:justify-between">
     <div className="min-w-0 flex-1">
       <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
-        Back End Developer
+        Backend Developer
       </h2>
       <div className="mt-1 flex flex-col sm:mt-0 sm:flex-row sm:flex-wrap sm:space-x-6">
         <div className="mt-2 flex items-center text-sm text-gray-500">
