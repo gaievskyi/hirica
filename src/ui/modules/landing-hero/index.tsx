d5f0a618dce3094f02d30d65960ffc3698d49376
Daniel Gajewski <daniel.gajewski@iteo.com>
2023-01-05 13:44:39 +0100

    Remove useless code

--- src/ui/modules/landing-hero/index.tsx
+++ src/ui/modules/landing-hero/index.tsx
@@ -5,7 +5,6 @@ import Link from 'next/link'
 
 export const LandingHero: React.FC = () => {
   const { data: sessionData } = useSession()
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
     <div className="isolate bg-[#fdfbf6]">
@@ -35,51 +34,6 @@ export const LandingHero: React.FC = () => {
             </linearGradient>
           </defs>
         </svg>
-      </div>
-      <div className="px-6 pt-6 lg:px-8">
-        <div>
-          <Dialog as="div" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
-            <Dialog.Panel
-              focus="true"
-              className="fixed inset-0 z-10 overflow-y-auto bg-white px-6 py-6 lg:hidden"
-            >
-              <div className="flex h-9 items-center justify-between">
-                <div className="flex">
-                  <a href="#" className="-m-1.5 p-1.5">
-                    <span className="sr-only">Your Company</span>
-                    <img
-                      className="h-8"
-                      src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                      alt=""
-                    />
-                  </a>
-                </div>
-                <div className="flex">
-                  <button
-                    type="button"
-                    className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-                    onClick={() => setMobileMenuOpen(false)}
-                  >
-                    <span className="sr-only">Close menu</span>
-                    {/* <XMarkIcon className="h-6 w-6" aria-hidden="true" /> */}
-                  </button>
-                </div>
-              </div>
-              <div className="mt-6 flow-root">
-                <div className="-my-6 divide-y divide-gray-500/10">
-                  <div className="py-6">
-                    <a
-                      href="#"
-                      className="-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-6 text-gray-900 hover:bg-gray-400/10"
-                    >
-                      Log in
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </Dialog.Panel>
-          </Dialog>
-        </div>
       </div>
       <main>
         <div className="relative px-6 lg:px-8">
