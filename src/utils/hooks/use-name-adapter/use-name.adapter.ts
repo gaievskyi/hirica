d2cf93c43684704556caa7a818be38ba0dda912a
Daniel Gajewski <daniel.gajewski@iteo.com>
2023-02-21 00:21:49 +0100

    Refactor use-name.adapter.

--- src/utils/hooks/use-name-adapter/use-name.adapter.ts
+++ src/utils/hooks/use-name-adapter/use-name.adapter.ts
@@ -1,9 +1,35 @@
-type FullName = string | null | undefined
+import { useMemo } from 'react'
+import { z } from 'zod'
 
-export function useNameAdapter(fullName: FullName) {
-  const fullNameArray: Array<string> = fullName?.split(' ') ?? []
-  const surname = fullNameArray.pop()
-  const name = fullNameArray.join(' ')
+const fullNameRegex = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/
 
-  return { fullName, name, surname }
+export const FullNameSchema = z.string().regex(fullNameRegex)
+
+type FullName = z.infer<typeof FullNameSchema>
+
+type FullNameParts = {
+  name: string
+  surname: string
 }
+
+const isValid = (fullName: FullName): boolean => {
+  const validatedFullName = FullNameSchema.safeParse(fullName)
+  return validatedFullName.success
+}
+
+const adapt = (fullName: FullName): FullNameParts => {
+  const fullNameArray = fullName.split(' ')
+  const surname = fullNameArray.slice(-1)[0] || ''
+  const name = fullNameArray.slice(0, -1).join(' ')
+  return { name, surname }
+}
+
+const useNameAdapter = (fullName: FullName): FullNameParts => {
+  return useMemo(
+    () => (isValid(fullName) ? adapt(fullName) : { name: '', surname: '' }),
+    [fullName]
+  )
+}
+
+export { useNameAdapter, fullNameRegex }
+export type { FullName, FullNameParts }
